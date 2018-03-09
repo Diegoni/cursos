d40347a0523ea0aa75186061cc580dffbb44b0bc
Diegoni <diego_nieto_1@hotmail.com>
2018-03-09 19:45:53 -0200

    2

--- application/config/my_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-
-/*---------------------------------------------------------------------------------
-        URL para los logs de la afip
----------------------------------------------------------------------------------*/
-
-$config['programa']             = 'colegio-notarial';
--- application/libraries/Barcores.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('BASEPATH')) exit('No direct script access allowed');  
- 
-require_once APPPATH."/third_party/php-barcode.php";
-
-class Barcores extends Barcode {
-
-}
