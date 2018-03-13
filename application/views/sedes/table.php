ce7e2bfd1f47021c11883ab2d75f5e06fe2b75d2
Diegoni <diego_nieto_1@hotmail.com>
2018-03-12 21:06:30 -0300

    2

---
+++ application/views/sedes/table.php
@@ -0,0 +1,59 @@
+<?php
+/*--------------------------------------------------------------------------------  
+            Comienzo del contenido
+ --------------------------------------------------------------------------------*/
+ 
+$cabeceras = array(
+    lang('nombre'),
+    lang('email'),
+    lang('abreviatura'), 
+    lang('opciones'), 
+);
+
+$html = startContent();
+
+if(isset($mensaje)){
+    $html .= setMensaje($mensaje);
+}
+
+/*--------------------------------------------------------------------------------  
+            Tabla
+ --------------------------------------------------------------------------------*/
+ 
+
+if($permiso_editar == 1)
+{
+    $html .= getExportsButtons($cabeceras, tableAdd($subjet));    
+}else
+{
+    $html .= getExportsButtons($cabeceras);
+}
+
+$html .= startTable($cabeceras);
+
+if($registros)
+{
+    foreach ($registros as $row) 
+    {
+        $registro = array(
+            $row->nombre,
+            $row->email,
+            $row->abreviatura,
+            tableUpd($subjet, $row->codSede).' '.tableButton('sedes/seleccion', $row->codSede, 'fa fa-arrow-right', lang('seleccionar')),
+        );
+        
+        $html .= setTableContent($registro);    
+    }
+}
+            
+$html .= endTable($cabeceras);         
+$html .= setDatatables();           
+
+/*--------------------------------------------------------------------------------  
+            Fin del contenido
+ --------------------------------------------------------------------------------*/
+ 
+$html .= endContent();
+
+echo $html;
+?>
