ae703677af6477bb10f3f73e1fc0b37c24d85eaa
Zoopsia <cristiannemesis101@hotmail.com>
2018-05-26 11:01:09 -0300

    - clean modal inscribir alumnos

--- application/views/cursos/curso.php
+++ application/views/cursos/curso.php
@@ -177,50 +177,12 @@
     </div>
 </section>
 
-
+<!--     modals      -->
 <?php include_once 'modal-cronogramas.php'; ?>
 <?php include_once 'modal-edit-curso.php'; ?>
 <?php include_once 'modal-inscribir-alumnos.php'; ?>
 
-<<<<<<< HEAD
-=======
-<!-- modal -->
-<div class="modal fade" id="modal-inscribir-alumnos" style="display: none;">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <form class="form-horizontal" method="post">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                    <h4 class="modal-title">Inscripción Alumnos </h4>
-                </div>
-                <div class="modal-body">
-                    <div class="form-group">
-                        <div class="col-xs-12 col-sm-12">
-                            <div class="input-group">
-                                <input type="text" class="form-control" id="inputBuscarAlumnos" placeholder="Buscar por Nombre y/o Apellido">
-                                <span class="input-group-btn">
-                                    <button class="btn btn-primary" id="buttonBuscarAlumnos" type="button">Buscar</button>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-xs-12 col-sm-12">
-                            <select class="form-control" multiple="multiple" id="selectInscAlumnos" name="curso_inscr_alumno[]">
-                                
-                            </select>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div>
->>>>>>> c478afadc2f78d6818a41f64d7ea3eb0cfff7691
-
-<!-- styles -->
+<!--    styles      -->
 <style>
     .actions-buttons button, 
     .actions-buttons a, 
